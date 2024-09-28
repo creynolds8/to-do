f201e73f9d27ba344e659636da193ebbd4eb7791
creynolds8 <cameron8.cr@gmail.com>
2024-09-28 15:12:31 -0700

    add form for adding new todos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import TodoForm from "./components/TodoForm";
 
 interface Todo {
   id: number;
@@ -11,17 +12,29 @@ const App = () => {
   const [ todos, setTodos ] = useState<Todo[]>([]);
 
   const fetchTodos = async () => {
-    const response = await axios.get('/api/todos');
-    setTodos(response.data);
+    try {
+      const response = await axios.get('/api/todos');
+      setTodos(response.data);
+    } catch (error) {
+      console.log('Error fetching Todos', error);
+      
+    }
   };
 
+  // useEffect, when passed an empty array will run only once when the page finishes loading
   useEffect(() => {
     fetchTodos();
   }, []);
 
+  // Handle new Todo 
+  const handleTodoAdded = (newTodo: Todo) => {
+    setTodos([...todos, newTodo])
+  }
+
   return (
       <div>
         <h1>To-Do List</h1>
+        <TodoForm onTodoAdded={handleTodoAdded} />
         <ul>
           {todos.map(todo => (
             <li key={todo.id}>
