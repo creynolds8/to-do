f91aa7775331b129dc2456af8de57ca1ec661a08
creynolds8 <cameron8.cr@gmail.com>
2024-10-16 16:44:40 -0700

    set up configuration for testing

---
+++ frontend/jest.config.mjs
@@ -0,0 +1,6 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleDirectories: ['node_modules', 'src'],
+};
