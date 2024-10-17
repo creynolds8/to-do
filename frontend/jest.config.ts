7faa9ec5722d22bb4eb5c1d00990a8bb61c7ec6d
creynolds8 <cameron8.cr@gmail.com>
2024-10-16 20:14:20 -0700

    more config changes to allow for assertions

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -1,7 +1,11 @@
-export default {
+import type {Config} from 'jest';
+
+const config: Config = {
   verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleDirectories: ['node_modules', 'src'],
 };
+
+export default config;
