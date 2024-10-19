621362fc70427e5bab4f963e6b2cd2df3eef6ecc
creynolds8 <cameron8.cr@gmail.com>
2024-10-18 21:23:42 -0700

    add basic test for rendering component

---
+++ frontend/src/tests/TodoDetails.test.tsx
@@ -0,0 +1,22 @@
+import { render } from "@testing-library/react";
+import TodoDetails from "../components/TodoDetails";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+
+describe("tests for TodoDetails component and functions", () => {
+  test("renders the component", () => {
+    const onToggleComplete = jest.fn();
+    const onUpdateTodo = jest.fn();
+    const onDeleteTodo = jest.fn();
+    const { getByText } = render(
+      <MemoryRouter initialEntries={["/todos/:id"]}>
+        <Routes>
+          <Route path={"/todos/:id"} element={
+            <TodoDetails onToggleComplete={onToggleComplete} onUpdateTodo={onUpdateTodo} onDeleteTodo={onDeleteTodo} />
+          } />
+        </Routes>
+      </MemoryRouter>
+    );
+    const renderCheck = getByText("Loading...")
+    expect(renderCheck).toBeInTheDocument();
+  });
+});
